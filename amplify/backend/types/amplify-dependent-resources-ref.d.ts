615f357e7b1a8d538ad9d814c0fd85301b54f60c
shwuen <lovebear2003@gmail.com>
2025-09-15 19:45:51 +0800

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apic6f3973d": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "amplifydemoapp33746fc0": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { get } from 'aws-amplify/api';   // ✅ new import
+import React, { useState } from 'react';
 
-function App() {
+const myAPI = "apic6f3973d";
+const path = '/customers'; 
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [customers, setCustomers] = useState([]);
+
+  async function getCustomer(e) {
+    let customerId = e.input;
+    try {
+      // ✅ build REST operation
+      const restOperation = get({
+        apiName: myAPI,
+        path: path + "/" + customerId
+      });
+
+      // ✅ wait for the response
+      const response = await restOperation.response;
+      const customer = await response.body.json();  // parse JSON body
+
+      console.log("GET call succeeded: ", customer);
+
+      setCustomers([...customers, customer]);
+    } catch (error) {
+      console.log("GET call failed: ", error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Super Simple React App</h1>
+      <div>
+        <input 
+          placeholder="customer id" 
+          type="text" 
+          value={input} 
+          onChange={(e) => setInput(e.target.value)}
+        />      
+      </div>
+      <br/>
+      <button onClick={() => getCustomer({input})}>
+        Get Customer From Backend
+      </button>
+
+      <h2 style={{visibility: customers.length > 0 ? 'visible' : 'hidden' }}>
+        Response
+      </h2>
+      {
+        customers.map((thisCustomer, index) => (
+          <div key={thisCustomer.customerId}>
+            <span>
+              <b>CustomerId:</b> {thisCustomer.customerId} - 
+              <b>CustomerName:</b> {thisCustomer.customerName}
+            </span>
+          </div>
+        ))
+      }
     </div>
-  );
+  )
 }
 
 export default App;
